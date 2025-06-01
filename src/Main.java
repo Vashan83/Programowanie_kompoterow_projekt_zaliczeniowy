8825ab6be8d357f50c793f73164296066a6ede13
ola-ozdzenska <olaozdz@gmail.com>
2025-06-01 10:50:36 +0000

    naprawienie struktury projektu, stworzenie PasswordDatabase,

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,17 @@ public class Main {
         String correctUsername = "Jan";
         String correctPassword = "Kowalski";
 
-        LogIn logowanie = new LogIn(correctUsername, correctPassword);
+        //zapisujemy jakie są poprawne dane
+        LogIn logowanie = new LogIn(correctUsername, correctPassword); 
 
+        //user wpisuje dane
         System.out.print("Podaj login: ");
         String userLogin = scanner.nextLine();
 
         System.out.print("Podaj hasło: ");
         String userPassword = scanner.nextLine();
 
+        //porównywanie danych systemowych z danymi od usera
         if (logowanie.authenticate(userLogin.trim(), userPassword.trim())) {
             System.out.println("Zalogowano pomyślnie jako: " + logowanie.getUsername());
         } else {
